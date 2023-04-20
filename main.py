5bfef5798893b8fecb263c65d1f2085e7309ac62
itsamaan <99782520+itsamaan@users.noreply.github.com>
2023-04-20 12:18:59 +0530

    Update main.py

--- main.py
+++ main.py
@@ -18,15 +18,20 @@ list_2 = [
 
 
 def merge_lists(list_1, list_2) -> list:
-    """
-    Complete this function, by merging the information from list_1 and list_2
-    to create a new list, which has all the information about each student from
-    both lists in one single dict.
-
-    - Both lists are unsorted
-    - Both lists can have missing values (for ex list_2 has missing id=2)
-    """
-    # return list_3
+    list_3 = []
+    # Merge student information from list_1 and list_2
+    for student in list_1:
+        merged_student = student.copy()
+        for student_2 in list_2:
+            if student_2.get('id') == student.get('id'):
+                merged_student.update(student_2)
+                break
+        list_3.append(merged_student)
+    # Add missing students from list_2 to list_3
+    for student_2 in list_2:
+        if not any(student_2.get('id') == student.get('id') for student in list_3):
+            list_3.append(student_2)
+    return list_3
 
 
 list_3 = merge_lists(list_1, list_2)
